407c4e25cb9e2cefecf7627fec91e9b54dc81ced
pauloprediger <142063372+pauloprediger@users.noreply.github.com>
2024-01-11 19:01:34 -0300

    vetores

--- Aulas/Aula016/script.js
+++ Aulas/Aula016/script.js
@@ -12,7 +12,7 @@ function isNumero(n){
     }
 }
 function inLista(n, l){
-    if (l.indexOf(n) != -1){
+    if (l.indexOf(Number(n)) != -1){
         return true
     }else{
         return false
@@ -20,16 +20,40 @@ function inLista(n, l){
 }
 
 function adicionar(){
+
     if(isNumero(txtNum.value) && !inLista(txtNum.value, vet)){
-        vet.push(txtNum.value)
+        vet.push(Number(txtNum.value))
+        let num = Number(txtNum.value)
         let item = document.createElement('option')
         item.text = `O valor ${txtNum.value} foi adicionado a lista`
         item.value = `sel${txtNum.value}`
         sel.appendChild(item)
-        window.alert(vet.length)
-        window.alert(vet)
+        res.innerHTML = ''
+
     }else{
         window.alert(`Valor invalido ou já consta na lista`)
     }
+    txtNum.value = ''
+    txtNum.focus()
 }
 
+function resultado(){
+    if(vet.length == 0){
+        window.alert(`Adicione Valores antes de finalizar`)
+    }else{
+        let maior = vet[0]
+        let menor = vet[0]
+        let soma = 0
+        for(let i in vet){
+            soma += vet[i]
+            if(vet[i] > maior){
+                maior = vet[i]
+            }
+            if(vet[i] < menor){
+                menor = vet[i]
+            }
+        }
+        res.innerHTML = ''
+        res.innerHTML += `Número adicionados: ${vet.length}<br> Soma dos valores: ${soma} <br> Média dos valores: ${soma/vet.length} <br> Maior número: ${maior} <br> Menor número: ${menor}`
+    }
+}
