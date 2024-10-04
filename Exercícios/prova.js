70087d058e0c346d87cdb1a334a810814ddf2a04
Paulo Victor Ribeiro Prediger <142063372+pauloprediger@users.noreply.github.com>
2024-10-03 21:03:26 -0300

    Mudando pastas

---
+++ Exercícios/prova.js
@@ -0,0 +1,12 @@
+const  forma = 'quadrado';
+const altura = 10;
+const comprimento = 14;
+
+if (forma === 'quadrado') {
+    area = (altura * comprimento) / 2;
+} else { 
+    'é uma forma geométrica';
+
+    let area;
+    console.log(area);
+}
